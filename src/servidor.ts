11e731abd7aafc8128c5b3fab93645b212738756
josiel <josiephelipel265@gmail.com>
2026-03-23 11:34:45 -0400

    3 arquivo servidor.ts

--- src/servidor.ts
+++ src/servidor.ts
@@ -4,6 +4,8 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import RotasUsuário from "./rotas/rotas-usuário";
 import RotasAutor from "./rotas/rotas-autor";
+import RotasGerenteEditora from "./rotas/rotas-gerente-editora";
+
 const app = express();
 const PORT = process.env.PORT
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
@@ -12,5 +14,6 @@ app.use(express.json());
 app.use("/usuarios", RotasUsuário);
 app.use("/autores", RotasAutor);
 app.listen(PORT || 3333);
+app.use("/gerentes-editoras", RotasGerenteEditora);
 const conexão = createConnection();
 export default conexão;
